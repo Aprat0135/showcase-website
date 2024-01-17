d3dfa25e2ef01355a110b7c1dc8bf8f294bd47cc
Philipp Patra <pm2001patra@gmail.com>
2024-01-17 16:30:03 +0100

    span instead of list

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,31 +1,19 @@
 import { Outlet, Link } from 'react-router-dom';
-import achtungBaustelle from './..\\img\\Achtung Baustelle.png';
+import ConstructionSign from './..\\img\\ConstructionSign.png';
 
 export default function Root() {
   return (
     <>
       <div class="divH1">
         <h1>Showcase Website</h1>
-        <img src={achtungBaustelle}/>
+        <img src={ConstructionSign} alt="ConstructionSign.png"/>
       </div>
-      <div id="nav">
-        <nav>
-          <ul>
-            <li>
-              <Link to={`/pages/home`}>Home</Link>
-            </li>
-            <li>
-              <Link to={"/pages/shop"}>Shop dummy</Link>
-            </li>
-            <li>
-              <Link to={"/pages/blog"}>Blog dummy</Link>
-            </li>
-            <li>
-              <Link to={"/pages/calender"}>Calender dummy</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <nav>
+        <span><Link to={`/pages/home`}>Home</Link></span>
+        <span><Link to={"/pages/shop"}>Shop dummy</Link></span>
+        <span><Link to={"/pages/blog"}>Blog dummy</Link></span>
+        <span><Link to={"/pages/calender"}>Calender dummy</Link></span>
+      </nav>
       <div id="detail">
         <Outlet />
       </div>
