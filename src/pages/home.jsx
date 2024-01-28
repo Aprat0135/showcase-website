261ec804e6e8a6c39d595385695c15ca46d4912d
Philipp Patra <pm2001patra@gmail.com>
2024-01-29 00:22:01 +0100

    solved nav-links allignement

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,14 @@
+import './../styles/Home.css';
+
 export default function Home() {
     return(
         <>
             <div>Moin... Und? ...öfter hier?</div>
+            <div class="container">
+                <div class="blue-block"></div>
+                <div class="green-block"></div>
+                <div class="black-block"></div>
+            </div>
         </>
     );
 }
